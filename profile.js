7f2566a611a877b3caee5b1e6521aafdf7c29c11
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-10-28 16:54:47 -0700

    created getUser and attempted 1st stretch goal

--- profile.js
+++ profile.js
@@ -25,10 +25,15 @@ formData.addEventListener('submit', function(event) {
     const user = makeUser(newData);
   //set data to local storage
 
-   
     saveUser(user);
 
-    window.location.replace();
+    //getUser from localStorage
+    const returnUser = getUser();
+    console.log(returnUser);
+    //save getUser to a variable
+    //if returnUser.name && password === newData.name && newData.password
+
+    window.location.replace(`./input-page.html?name=${returnUser.name}`);
 });
 
 
@@ -40,9 +45,15 @@ function makeUser(formData) {
         password: formData.get('pass'),
         todonot: [], 
     };
-
 }
+
 function saveUser(makeUser) {
     const json = JSON.stringify(makeUser);
     localStorage.setItem('USER', json);
 }
+
+function getUser() {
+    const userString = localStorage.getItem('USER');
+    return JSON.parse(userString);
+}
+
